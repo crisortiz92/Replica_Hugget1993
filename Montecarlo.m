efcd965596a55ac5af37b82c2fb472eec07ea8bd
crisortiz92 <55200752+crisortiz92@users.noreply.github.com>
2019-12-24 20:20:07 -0500

    Update Montecarlo.m

--- Montecarlo.m
+++ Montecarlo.m
@@ -10,7 +10,7 @@
 %                           
 %           OPTIMIZATION: USING MONTECARLO SIMULATION
 %
-%                                CRISTIAN ORTIZ
+%                                CRISTHIAN ORTIZ
 %                                PAUL PONCE
 %                                KEVIN ROJAS
 %                                SANTIAGO SANDOVAL
